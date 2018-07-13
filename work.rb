02481d78c27ada7d4389d06cf8ef823d780e68b4
River Pease <riverpease15@gmail.com>
2018-07-13 19:33:39 +0000

    automatically backed up by learn

--- work.rb
+++ work.rb
@@ -1,18 +1,5 @@
-class Dog 
-  
-  def initialize(name, breed)
-    @name = name
-    @breed = breed 
-  end
-  
-  public
-  def bark
-    puts "Woof!"
-  end
-  
-  private
-  def id 
-    @id_number = 12345
-  end 
+module MyLibrary
+ 
+ FAVE_BOOK = "Bad News"
   
 end
